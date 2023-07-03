8d178b5e84da7d28dc1204e9c21733b0b4059a4a
Matheus <mathgomes16@gmail.com>
2023-07-02 22:25:04 -0300

    Lista de Atividades

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit{
 
+  taskList: Array<TaskList> = [
+    {checked: true, task: "teste"}
+  ];
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+  public deletarItem(event: number){
+    if(window.confirm("Tem certeza que deseja apagar essa tarefa?")){
+      this.taskList.splice(event, 1);
+    }
+
+  }
+
+  public deletarTudo(){
+    if(window.confirm("Tem certeza que deseja deletar todas as tarefas?")){
+      this.taskList = [];
+    }
+  }
 }
